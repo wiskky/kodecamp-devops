0e57663d8da27bca160e6f3ae57a652342421954
wiskky <adeniyi.wisdom@gmail.com>
2025-08-29 22:19:44 +0100

    correcting error in instance type

--- terraform/main.tf
+++ terraform/main.tf
@@ -141,7 +141,7 @@ data "aws_ami" "ubuntu" {
 # ---------- EC2 ----------
 resource "aws_instance" "app" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.dream.id
   vpc_security_group_ids      = [aws_security_group.web.id]
   associate_public_ip_address = true
