3e03c8151adb5cae29becb38ea7c05ab5ce02321
wiskky <adeniyi.wisdom@gmail.com>
2025-08-29 20:56:33 +0100

    updating files

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,7 +67,7 @@ resource "aws_security_group" "web" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
+  }
   ingress {
     description = "Custom application port"
     from_port   = 3000
