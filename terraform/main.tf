f0768551bb38833ccffa9447fa085a9a753e2b1c
wiskky <adeniyi.wisdom@gmail.com>
2025-09-03 06:31:51 +0100

    I reset back to updating compose 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,6 @@ resource "aws_subnet" "dream_subnet" {
   vpc_id                  = aws_vpc.dream_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "${var.aws_region}a"
   tags = { Name = "dream-subnet" }
 }
 
