48152a2750dfdc0ceb0d469a55a2ab245c9e0300
wiskky <adeniyi.wisdom@gmail.com>
2025-09-07 22:06:37 +0100

    deleting duplicate output for pulic ip

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,6 @@ output "instance_id" {
   value = aws_instance.dream.id
 }
 
-output "public_ip" {
-  value = aws_instance.dream.public_ip
-}
-
 output "ssh_command" {
   value = "ssh -i ~/.ssh/dream-key.pem ubuntu@${aws_instance.dream.public_ip}"
 }
