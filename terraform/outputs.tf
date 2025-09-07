1cc2b4911507c0e1adcb5f2213aa7e6fefdb7be8
wiskky <adeniyi.wisdom@gmail.com>
2025-09-07 21:54:37 +0100

    Updating public IP for ec2 key

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,8 @@ output "public_ip" {
 output "ssh_command" {
   value = "ssh -i ~/.ssh/dream-key.pem ubuntu@${aws_instance.dream.public_ip}"
 }
+
+output "public_ip" {
+  description = "Public IP of EC2 instance"
+  value       = aws_instance.dream.public_ip
+}
